d459302bbab73b278f01e397cbdda28a57a8a140
Ashish473797 <ashish473797@gmail.com>
2023-09-04 19:09:16 +0530

    updated

--- 06-Resume-Builder/src/Resume.js
+++ 06-Resume-Builder/src/Resume.js
@@ -1,22 +1,20 @@
 import './Resume.css';
-// import userData from './assets/data';
-// import Header from './components/header';
-// import LeftHeadings from './components/leftHeadings';
-// import RighttHeadings from './components/rightHeadings';
-import Timer from './timer/timer';
+import userData from './assets/data';
+import Header from './components/header';
+import LeftHeadings from './components/leftHeadings';
+import RighttHeadings from './components/rightHeadings';
 
 function Resume() {
   return (
     <>
-      <Timer></Timer>
-      {/* <div className='print' onClick={()=>{window.print()}}>Print</div>
+      <div className='print' onClick={()=>{window.print()}}>Print</div>
       <div className='page'>
         <Header {...userData}/>
         <div className='flex'>
         <LeftHeadings prop={userData}/>
         <RighttHeadings prop={userData}/>
         </div>
-      </div> */}
+      </div>
     </>
   );
 }
